b19b7c386e6cacc768f0341f15d287283860fa16
Loci <l_natit@outlook.com>
2026-05-17 19:27:25 +0800

    Fix FD recipe tag

--- compat-farmersdelight/src/main/java/com/lnatit/ccw/datagen/FarmersDelightRecipeProvider.java
+++ compat-farmersdelight/src/main/java/com/lnatit/ccw/datagen/FarmersDelightRecipeProvider.java
@@ -8,13 +8,11 @@ import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.common.conditions.ModLoadedCondition;
 import vectorwing.farmersdelight.client.recipebook.CookingPotRecipeBookTab;
 import vectorwing.farmersdelight.data.builder.CookingPotRecipeBuilder;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Stream;
 
 public class FarmersDelightRecipeProvider extends RecipeProvider {
     protected FarmersDelightRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
@@ -27,11 +25,7 @@ public class FarmersDelightRecipeProvider extends RecipeProvider {
 
         CookingPotRecipeBuilder.cookingPotRecipe(FarmersDelightCompats.GLAZED_MEAT_RICE.get(), 1, 200, 1.0F)
                 .addIngredient(ItemRegistry.GUMMY)
-                .addIngredient(Ingredient.fromValues(Stream.of(
-                        new Ingredient.TagValue(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "foods/raw/chicken"))),
-                        new Ingredient.TagValue(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "foods/raw/pork"))),
-                        new Ingredient.TagValue(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "foods/raw/beef")))
-                )))
+                .addIngredient(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "foods/raw_meat")))
                 .addIngredient(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "crops/rice")))
                 .addIngredient(ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "foods/leafy_green")))
                 .unlockedBy("has_gummy", has(ItemRegistry.GUMMY))
